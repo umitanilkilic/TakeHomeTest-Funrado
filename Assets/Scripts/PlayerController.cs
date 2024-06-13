502a90aa2ecf8ecc0b653d5ba43b053fdcf6f775
Anil <umitanilkilic@gmail.com>
2024-06-13 21:41:47 +0300

    player controller updated

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     public FixedJoystick joystick;
-    public float gravity = 9.8f;
-    public float speed = 3.0f;
+    public float movementSpeed = 5f;
+    public float gravity = -9.81f;
+    public float rotationSpeed = 10f;
+    public float smoothTurnTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    public TextMeshProUGUI levelText;
+
     private CharacterController characterController;
-    void Awake()
+    private int playerLevel = 1;
+    private Vector3 velocity;
+
+    public Animator playerAnimator;
+
+    public int PlayerLevel
+    {
+        get => playerLevel;
+        set
+        {
+            playerLevel = value;
+            levelText.text = $"Lv. {playerLevel}";
+        }
+    }
+
+    private void Awake()
     {
         characterController = GetComponent<CharacterController>();
+        levelText = GetComponentInChildren<TextMeshProUGUI>();
     }
-    
-    void Update()
+
+    private void Start()
     {
-        var velocity = new Vector3(joystick.Horizontal,-gravity,joystick.Vertical);
-        characterController.Move(velocity*Time.deltaTime*speed);
+        PlayerLevel = 1;
     }
+
+    private void Update()
+    {
+        MoveAndRotateCharacter();
+    }
+
+    private void MoveAndRotateCharacter()
+    {
+        float h = joystick.Horizontal;
+        float v = joystick.Vertical;
+
+        Vector3 moveDirection = new Vector3(h, 0f, v);
+
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            characterController.Move(transform.forward * movementSpeed * Time.deltaTime);
+            playerAnimator.Play("Run");
+        }
+        else
+        {
+            playerAnimator.Play("Idle");
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
 }
